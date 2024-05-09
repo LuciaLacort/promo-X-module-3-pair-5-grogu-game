4311c55a6a00b2119b37fdac4e1e4d05f43cd774
Diana <dianaga93@hotmail.com>
2024-05-09 11:21:42 +0200

    estructure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,35 @@
 import "../styles/App.scss";
-
+// 
 function App() {
+  const [grogu,setGrogu]= useState ("");
+  const [coockie,setCoockie]= useState ("");
+  const [egg,setEgg]= useState ("");
+  const [frog,setFrog]= useState ("");
+  const [dice,setDice]= useState ("");
+  const [result,setresult]= useState ("");
+  // Función que oculte divs de las mercancias o mover los pies +1 casilla
+  const getRandomNumber = (max) =>{
+    return Math.ceil(Math.random() * max);
+  }
+
+  const getDiceMove = () =>{
+    //segun nº que obtengamos tendremos un movimiento
+  }
+
+  const handleClick = () =>{
+
+  }
+
   return (
     <>
     <header>
       <h1>¡Cuidado con Grogu!</h1>
     </header>
     <main className="page">
+      <p>Introduce tu nombre para jugar</p>
+      <form action="">
+        <input type="text" name= "name" />
+      </form>
       <section className="board">
         <div className="cell"><div className="grogu">👣</div></div>
         <div className="cell"></div>
@@ -18,7 +41,7 @@ function App() {
       </section>
 
       <section>
-        <button className="dice">Lanzar Dado</button>
+        <button className="dice" onClick={handleClick} >Lanzar Dado</button>
         <div className="game-status">En curso</div>
       </section>
 
